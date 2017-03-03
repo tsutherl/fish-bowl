809e12622fd97f02a7d907cf04595aedfb61bdad
Ally Cody <allysoncody2015@u.northwestern.edu>
2017-03-03 15:38:27 -0500

    initialize firebase in main

--- app/main.jsx
+++ app/main.jsx
@@ -8,25 +8,36 @@ import store from './store'
 import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
+import keys from 'APP/keys.js'
+import firebase from 'firebase'
 
-const ExampleApp = connect(
-  ({ auth }) => ({ user: auth })
-) (
-  ({ user, children }) =>
-    <div>
-      <nav>
-        {user ? <WhoAmI/> : <Login/>}
-      </nav> 
-      {children}
-    </div>
-)
+const config = keys.config
+
+firebase.initializeApp(config)
+
+const database = firebase.database();
+const auth = firebase.auth();
+
+
+// const ExampleApp = connect(
+//   ({ auth }) => ({ user: auth })
+// ) (
+//   ({ user, children }) =>
+//     <div>
+//       <nav>
+//         {user ? <WhoAmI/> : <Login/>}
+//       </nav> 
+//       {children}
+//     </div>
+// )
+
+
+
 
 render (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={ExampleApp}>
-        <IndexRedirect to="/jokes" />
-        <Route path="/jokes" component={Jokes} />
+      <Route path="/" component={HomeScreen}>
       </Route>
     </Router>
   </Provider>,
