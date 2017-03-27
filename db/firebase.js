9413980ecbc02cd280ecd6641c1a15814dc1ebe1
Tatiana <sutherland.tati@gmail.com>
2017-03-27 16:05:18 -0400

    deleted firebase file

--- db/firebase.js
+++
@@ -1,9 +0,0 @@
-import keys from 'APP/keys.js'
-import firebase from 'firebase'
-
-const config = keys.config
-
-export default firebase.initializeApp(config)
-
-export const database = firebase.database();
-export const auth = firebase.auth();
